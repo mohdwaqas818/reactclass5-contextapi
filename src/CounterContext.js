99a7091c47ea86f779783c426701b225617b88fa
Muhammad Waqas <mohdwaqas818@users.noreply.github.com>
2020-10-24 11:59:46 +1100

    Update CounterContext.js

--- src/CounterContext.js
+++ src/CounterContext.js
@@ -1,5 +1,11 @@
 import React, {createContext} from 'react';
+import ReactDOM from 'react-dom';
 
 const CounterContext = createContext(10);
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+)
 
 export default CounterContext;
